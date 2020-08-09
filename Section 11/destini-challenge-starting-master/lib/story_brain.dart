74eb9a7b55c7cfa9e2b970b01d439c80c6786dc8
Achmad Fadhitya Muharram <afadhitya@gmail.com>
2020-08-09 21:52:19 +0700

    [ section 11 ] make storynumber as a private property

--- Section 11/destini-challenge-starting-master/lib/story_brain.dart
+++ Section 11/destini-challenge-starting-master/lib/story_brain.dart
@@ -7,7 +7,7 @@
 import 'story.dart';
 
 class StoryBrain {
-  int storyNumber = 0;
+  int _storyNumber = 0;
 
   List<Story> _storyData = [
     Story(
@@ -45,40 +45,40 @@ class StoryBrain {
 
 //TODO: [x] Step 8 - Create a method called getStory() that returns the first storyTitle from _storyData.
   String getStory() {
-    return _storyData[storyNumber].storyTitle;
+    return _storyData[_storyNumber].storyTitle;
   }
 
 //TODO: [x] Step 11 - Create a method called getChoice1() that returns the text for the first choice1 from _storyData.
 
   String getChoice1() {
-    return _storyData[storyNumber].choice1;
+    return _storyData[_storyNumber].choice1;
   }
 
 //TODO: [x] Step 12 - Create a method called getChoice2() that returns the text for the first choice2 from _storyData.
   String getChoice2() {
-    return _storyData[storyNumber].choice2;
+    return _storyData[_storyNumber].choice2;
   }
 
   void nextStory(int choiceNumber) {
-    if (choiceNumber == 1 && storyNumber == 0) {
-      storyNumber = 2;
-    } else if (choiceNumber == 2 && storyNumber == 0) {
-      storyNumber = 1;
-    } else if (choiceNumber == 1 && storyNumber == 1) {
-      storyNumber = 2;
-    } else if (choiceNumber == 2 && storyNumber == 1) {
-      storyNumber = 3;
-    } else if (choiceNumber == 1 && storyNumber == 2) {
-      storyNumber = 5;
-    } else if (choiceNumber == 2 && storyNumber == 2) {
-      storyNumber = 4;
-    } else if (storyNumber == 3 || storyNumber == 4 || storyNumber == 5) {
+    if (choiceNumber == 1 && _storyNumber == 0) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 0) {
+      _storyNumber = 1;
+    } else if (choiceNumber == 1 && _storyNumber == 1) {
+      _storyNumber = 2;
+    } else if (choiceNumber == 2 && _storyNumber == 1) {
+      _storyNumber = 3;
+    } else if (choiceNumber == 1 && _storyNumber == 2) {
+      _storyNumber = 5;
+    } else if (choiceNumber == 2 && _storyNumber == 2) {
+      _storyNumber = 4;
+    } else if (_storyNumber == 3 || _storyNumber == 4 || _storyNumber == 5) {
       restart();
     }
   }
 
   void restart() {
-    storyNumber = 0;
+    _storyNumber = 0;
   }
 
 //TODO: Step 25 - Change the storyNumber property into a private property so that only story_brain.dart has access to it. You can do this by right clicking on the name (storyNumber) and selecting Refactor -> Rename to make the change across all the places where it's used.
